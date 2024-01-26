6046496665889e1725b55476e155dbd5fb14c81e
xd-Abi <abinayansureskumar@gmail.com>
2024-01-27 00:44:43 +0100

    Readded basic astra search engine

--- astra/web/src/app/download/page.tsx
+++ astra/web/src/app/download/page.tsx
@@ -38,8 +38,8 @@ export default function DownloadPage() {
               className="w-6 h-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
               />
             </svg>
