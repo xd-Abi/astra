f7d4945d8e13bebf1c1ee1dd79685280760319cc
xd-Abi <abinayansureskumar@gmail.com>
2024-02-01 23:36:52 +0100

    Fixed playwright not starting frontend issue

--- astra/web/tests/page.spec.ts
+++ astra/web/tests/page.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('redirecting to Google after searching', async ({ page }) => {
-  await page.goto('http://localhost:3000/', { waitUntil: 'networkidle' });
+  await page.goto('http://localhost:4000/', { waitUntil: 'networkidle' });
 
   const searchInputSelector = 'input';
   await page.fill(searchInputSelector, 'marwin');
